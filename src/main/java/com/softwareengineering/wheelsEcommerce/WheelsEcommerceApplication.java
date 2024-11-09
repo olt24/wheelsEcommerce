89109d8f2e444e5557620a1fc278f51e55b51e84
olt24 <kotorriolti4@gmail.com>
2024-11-09 13:25:16 +0100

    Initial commit

---
+++ src/main/java/com/softwareengineering/wheelsEcommerce/WheelsEcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.softwareengineering.wheelsEcommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WheelsEcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WheelsEcommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/softwareengineering/wheelsEcommerce/WheelsEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softwareengineering.wheelsEcommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WheelsEcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
